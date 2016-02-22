cea1cc40a8e027370210d36978c77c097667aa9f
Dominique Remy <dominique.remy@teachforamerica.org>
2016-02-22 00:21:22 -0800

    Yo Angular...

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name buildboardApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the buildboardApp
+ */
+angular.module('buildboardApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name buildboardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the buildboardApp
+ */
+angular.module('buildboardApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
