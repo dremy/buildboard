ab085ec1c8c43dc77a2455d8654e9cf36f046a15
Dominique Remy <dominique.remy@teachforamerica.org>
2016-03-04 20:35:33 -0800

    Added a loading.html view and updated considerations for the model.

--- app/investment.js
+++ app/investment.js
@@ -9,6 +9,7 @@ var investment = {
       {
         "lid": "25814", //Loop ID = NID
         "title": "Dremy Investments, LLC" //investment.details.loop[0].title
+        "logo": "http://photos1.zillowstatic.com/p_d/ISp98yfgbxdzrt0000000000.jpg"
       }
     ],
     "acquisition": {
@@ -25,8 +26,8 @@ var investment = {
       "targetExitDate": "1990-11-14T00:39:54.081Z",
       "listPrice": 320000,
       "listDate": "1990-11-14T00:39:54.081Z",
-      "exitDate": null,
-      "exitAmount": 329000,
+      "date": null,
+      "amount": 329000,
       "targetValue": 32000,
       "targetTakeHome": 45000,
       "closingPercent": 0.06,
@@ -113,7 +114,131 @@ var investment = {
   }
 }
 
-/*List View
+/* ADD INVESTMENT - PROPERTY
+investment
+  .type = [property], note, loop
+
+
+
+  
+ */
+
+/* ADD INVESTMENT - NOTE
+investment
+  .type = property, [note], loop
+
+
+
+  
+ */
+var investmentLoopCorporation = {
+  "investment": {
+    "id": "14894",
+    "type": "loop", 
+    "title": "drēmy Investments, LLC",
+    "logo": "http://photos1.zillowstatic.com/p_d/ISp98yfgbxdzrt0000000000.jpg",
+    "acquisition": {
+      "date": null,
+      "currency": "USD",
+      "invested": 22000, //sum of each(investment.details.payments[0].amount);       
+    },
+    "exit": {
+      "date": null,
+      "amount": 20000,
+    },
+    "loop": {
+      "type": "corporation", //investment.loop.type
+      "corporationType": "LLC",
+      "totalShares": 1000,
+      "users": {
+        "singular": "member", //investment.loop.users.singular
+        "plural": "members",
+        "count": "4",
+        "user": [
+          {
+            "uid": "2", //investment.loop.users.user[0].uid
+            "share": .25,
+            "payment": 5500
+          },
+          {
+            "uid": "527",
+            "share": .25,
+            "payment": 5500
+          },            
+          {
+            "uid": "793",
+            "share": .25,           
+            "payment": 5500
+          },
+          {
+            "uid": "1416",
+            "share": .25,
+            "payment": 5500
+          },
+        ]
+      }
+    }
+  }
+}
+
+// Conditional logic to set in the event
+function (investment) {
+  switch (this.type) {
+    // If its a property.
+    case "property":
+      break;
+    // If its a note.
+    case "note":
+      break;
+    // If its a loop.
+    case "loop":
+      // Set User Count.
+      for (var i = 0; i < this.loop.users.user.length; i++) {
+        this.loop.users.count = i;
+      };
+      // If the Loop type is...
+      switch (this.loop.type) {
+        case "casual":
+          // Set the language to use for users.
+          this.loop.users.singular = "person";
+          this.loop.users.plural = "people";        
+          break;
+        case "corporation":
+          var incorporationType = ["LLC", "S-Corps", "C-Corps"];
+          this.loop.users.singular = "member";
+          this.loop.users.plural = "members";
+          break;
+        case "partnership":
+          var incorporationType = ["LP", "LLP", "GP"];
+          this.loop.users.singular = "partner";
+          this.loop.users.plural = "partners";
+          break;
+      }
+      break;
+  };  
+}
+
+/* ADD INVESTMENT - LOOP
+investment
+  .type = property, note, [loop]
+  .title = "Dremy Investments, LLC"
+  .logo = "http://photos1.zillowstatic.com/p_d/ISp98yfgbxdzrt0000000000.jpg"
+  .loop
+    .type = casual, sole propertiership, corporation, partnership
+    .incorporationType = LLC, S-Corps, C-Corps, LP, LLP
+    .user
+      .singular = person, member, partner
+      .plural = people, members, partners
+      .count = 4
+        .users[0]
+
+
+
+
+  
+ */
+
+/* LIST INVESTMENT
 investment
   .title
   .iid
