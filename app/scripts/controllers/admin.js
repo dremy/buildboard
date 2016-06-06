9f9d3989cb966d492b39baa9610a65be17da52c2
Dominique Remy <dominique.remy@teachforamerica.org>
2016-06-06 01:44:19 -0700

    Persistent authentication and working logged out.

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -7,12 +7,29 @@
  * # AdminCtrl
  * Controller of the buildboardApp
  */
-function AdminCtrl($scope, currentSpot) {
+function AdminCtrl($scope, $rootScope, $location, currentSpot, drupal) {
   this.awesomeThings = [
     'HTML5 Boilerplate',
     'AngularJS',
     'Karma'
   ];
+
+  $rootScope.globals = {};   //Set globals
+  $rootScope.globals.isLoading = true;   //Set preloader
+  $scope.message = ''; // Set empty message
+ 
+  //Check if already authenticated
+  drupal.connect().then(function(data) {
+    if (data.user.uid) {
+      $rootScope.globals.currentUser = data.user;
+      $scope.currentUser = data.user; 
+      $scope.message = 'Hello ' + data.user.name + '!'; 
+      console.log(data.user.uid + " " + data.sessid + " " + data.session_name);
+    } else { 
+      console.log('Please login.');  
+    }
+    $rootScope.globals.isLoading = false;
+  });
 
   function isActive(menuId) {
     return currentSpot.getActiveMenu() === menuId;
@@ -22,9 +39,40 @@ function AdminCtrl($scope, currentSpot) {
     return currentSpot.getTitle();
   }
 
+  function isLoggedIn() {
+    if($rootScope.globals.currentUser) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function isLoading() {
+    if($rootScope.globals.isLoading === true) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function logout() {
+    $rootScope.globals.isLoading = true;
+    drupal.user_logout().then(function(data) {
+      if (!data.user.uid) {
+        alert('You have been logged out.');
+        $rootScope.globals = {};
+        $location.path('/app');
+        $scope.message = 'You have been logged out.';
+      }
+    });
+  }
+
   // Register the functions.
   $scope.isActive = isActive;
-  $scope.getTitle = getTitle;  
+  $scope.getTitle = getTitle;
+  $scope.isLoggedIn = isLoggedIn;
+  $scope.isLoading = isLoading;
+  $scope.logout = logout;
 }
 
 angular.module('buildboardApp')
