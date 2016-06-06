39521899007d0f44dbff2e6c63c42030841491db
Dominique Remy <dominique.remy@teachforamerica.org>
2016-06-06 01:51:17 -0700

    Updated drupalNg module.

--- app/scripts/modules/drupalNg.js
+++ app/scripts/modules/drupalNg.js
@@ -1,9 +1,26 @@
+'use strict';
+
 // The angular-drupal configuration settings for my simple app.
 angular
-  .module('angular-drupal').config(function($provide) {
-    // Name our endpoint for Angular Drupal
-    $provide.value('drupalSettings', {
-      sitePath: 'http://api.buildboard.io',
-      endpoint: 'properties/v1'
-    });
-  });
+  .module('angular-drupal')
+    .config(function($provide) {
+      // Name our endpoint for Angular Drupal
+      $provide.value('drupalSettings', {
+        sitePath: 'http://api.buildboard.io',
+        endpoint: 'v1/api'
+      });
+    })
+    .config(['$httpProvider',
+      function($httpProvider) {
+        $httpProvider.interceptors.push(['$q',
+          function($q) {
+            return {
+              request: function(config) {
+                config.withCredentials = true;
+                return config;
+              }
+            };
+          }
+        ]);
+      }
+    ]);
