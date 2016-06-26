80c8ffb1f8effaea5378dfbf33253e4de8a7b7c9
Dominique Remy <dominique.remy@teachforamerica.org>
2016-06-26 12:47:32 -0700

    Added alerts and refactored console.logs from property controller.

--- app/scripts/controllers/property.js
+++ app/scripts/controllers/property.js
@@ -10,19 +10,28 @@
 function propertyCtrl($scope, $rootScope, $routeParams, drupal) { 
 
   $rootScope.globals.isLoading = true;
-
   var nid = $routeParams.nid;
 
-  console.log(nid);
-  // Load node clicked
+  //Alert.
+  function alerting(message, type) {// TO DO - Global solve.
+    $scope.$emit('alert', { // Emit message.
+      message: message,
+      type: type,
+    });
+    $rootScope.globals.isLoading = false;
+  }
+
+  //Load node clicked.
   drupal.node_load(nid).then(function(node) {
       // Assign to scope
       $scope.property = node;
-      console.log($scope.property);
       $rootScope.globals.isLoading = false;
   }, function(reason) {
-    //console.log('Adding failed due to ' + reason.status + reason.statusText + '. Try again later.');
-    $rootScope.globals.isLoading = false;
+
+    //Alert.
+    var message = 'Adding failed due to ' + reason.statusText + '. Try again later.';
+    var type = 'warning';
+    alerting(message, type);
   });
 }
 
