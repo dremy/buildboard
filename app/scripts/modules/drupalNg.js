e327fc6522f44dd9108cede08b08e32569d08667
Dominique Remy <dominique.remy@teachforamerica.org>
2016-07-11 08:44:48 -0700

    location based if then statement for drupalNg.js

--- app/scripts/modules/drupalNg.js
+++ app/scripts/modules/drupalNg.js
@@ -1,12 +1,33 @@
 'use strict';
 
+// ===========================================================
 // The angular-drupal configuration settings for my simple app.
+
+//Initial Config
+//------------------------------------
+var sitePath = '';
+if (location.host === 'localhost') {
+  sitePath = 'http://localhost/bb_pantheon/buildboard/';
+} else {
+  console.log(location.host);
+  sitePath = 'http://dev-buildboard.pantheonsite.io/';
+}
+
+//TO DO - Move to Switch statement
+/*switch (location.host) {
+  case 'localhost':
+    sitePath = 'http://localhost/bb_pantheon/buildboard/';
+  break;
+}*/
+
+//Setup Angular Drupal
+//------------------------------------
 angular
   .module('angular-drupal')
     .config(function($provide) {
       // Name our endpoint for Angular Drupal
       $provide.value('drupalSettings', {
-        sitePath: 'http://api.buildboard.io',
+        sitePath: sitePath,
         endpoint: 'v1/api'
       });
     })
