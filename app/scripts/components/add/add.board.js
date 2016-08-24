188bb42468882507c59cb5e981823f4e2950d518
Dominique Remy <dominique.remy@teachforamerica.org>
2016-08-24 10:15:21 -0700

    Moved add.board.js to add component.

---
+++ app/scripts/components/add/add.board.js
@@ -0,0 +1,99 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bb.board.controller:addBoardCtrl
+ * @description
+ * # AddBoardCtrl
+ * Controller of the bb.board
+ */
+function addBoardCtrl($state, $scope, filepickerService, relationshipService, boardService, fileService, rel, messages, alert, preloader) { 
+  // Initialize variables.
+  //-------------------------------  
+  var board = this;
+  board.data = {
+    title: '',
+    _files: [],
+    _tags: '',
+    desciption: ''
+  };
+  board.files = {};
+  var relationship = rel.data[0];
+  board.relationship = relationship;
+  // Define functions.
+  //-------------------------------
+  //Multiple files upload set to 3 as max number
+  function uploadMultiple() {
+    filepickerService.pickMultiple(
+      {
+        language: 'en',
+        //maxFiles: 3, //pickMultiple has one more option
+        services: ['COMPUTER','URL','DROPBOX','GOOGLE_DRIVE','IMAGE_SEARCH', 'FACEBOOK', 'INSTAGRAM'],
+        openTo: 'COMPUTER'
+      },
+      function(Blob){
+        board.files = Blob;
+        $scope.$apply();
+        saveFiles(Blob);
+      }
+    );
+  }
+
+  function saveFiles(Blob) {
+    console.log('Type of Blob', typeof Blob);
+    for (var i in Blob) {
+      delete Blob[i].id; // No need to store id.
+      fileService.addFile(Blob[i])
+        .success(
+          function(file) {
+            board.data._files.push(file._id);
+          }
+        );
+    }
+  }
+
+  function saveBoard() {
+    board.data._tags = $('#chips').material_chip('data');
+    // Set the relationship ID
+    board.data._relationship = relationship._id;
+    // Create the board.
+    boardService.addBoard(board.data)
+      .success(
+        function(data) {
+          if (!data.error) {
+            console.log('Created Board', data);
+            var update = {
+              _boards: relationship._boards,
+              _id: data._relationship || relationship._id
+            };
+            update._boards.push(data._id);
+            console.log('Update Data', update);
+            relationshipService.updateRelationship(update)
+              .success(function(data) {
+                console.log('Updated Relationship', data);
+              })
+              .then(function() {
+                $state.go('property', {id: relationship._property._id})
+              });
+          } else {
+            console.log('Failed', data);
+          }
+        }
+      )
+      .error(function(data) {
+        console.log('Failed', data);
+      });
+  }
+  // Perform on load.
+  //-------------------------------
+  $('#chips').material_chip({
+    placeholder: 'Enter a tag',
+    secondaryPlaceholder: 'Add a tag. Press Enter.',
+    data:[]
+  });
+  board.saveBoard = saveBoard;
+  board.uploadMultiple = uploadMultiple;
+}
+
+angular.module('bb.board', [])
+  .controller('AddBoardCtrl', addBoardCtrl);
